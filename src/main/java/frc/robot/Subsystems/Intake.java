2759a6b2a61aee60e49ff1fb0c4d8923a8351cc3
Arhaan28 <arhaanreddy@gmail.com>
2024-12-07 21:00:42 -0800

    Fixed Intake Subsystem errors

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,13 +1,21 @@
 package frc.robot.Subsystems;
 
+import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Intake {
-    private CANSparkMax intakeMotorOne = new CANSparkMax(1);
-    private CANSparkMax intakeMotorTwo = new CANSparkMax(2);
+public class Intake extends SubsystemBase{
+    private final CANSparkMax intakeMotorOne = new CANSparkMax(1, CANSparkLowLevel.MotorType.kBrushless);
+    private final CANSparkMax intakeMotorTwo = new CANSparkMax(2, CANSparkLowLevel.MotorType.kBrushless);
+
+    private class IntakeInfo extends SubsystemBase{
+        @SuppressWarnings("unused")
+        private double intakeMotorVelocity=0.0;
+        @SuppressWarnings("unused")
+        private double intakeMotorPosition=0.0;
+    }
 
     public void runIntakeMotors(double power) {
         intakeMotorOne.set(power);
@@ -15,28 +23,49 @@ public class Intake {
     }
 
     public void coast() {
-        intakeMotorOne.setNeutralMode(NeutralModeValue.Coast);
-        intakeMotorTwo.setNeutralMode(NeutralModeValue.Coast);
+        intakeMotorOne.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        intakeMotorTwo.setIdleMode(CANSparkMax.IdleMode.kCoast);
     }
 
     public void brake() {
-        intakeMotorOne.setNeutralMode(NeutralModeValue.Brake);
-        intakeMotorTwo.setNeutralMode(NeutralModeValue.Brake);
+        intakeMotorOne.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        intakeMotorTwo.setIdleMode(CANSparkMax.IdleMode.kBrake);
     }
 
-    public double getIntakeInfo() {
-        return  intakeMotorOne.getPosition().getValueAsDouble(), 
-        intakeMotorOne.getVelocity().getValueAsDouble(), 
-        intakeMotorTwo.getPosition().getValueAsDouble(),
-        intakeMotorTwo.getVelocity().getValueAsDouble();
+    public IntakeInfo getIntakeMotorOneInfo() {
+        IntakeInfo one = new IntakeInfo();
+        RelativeEncoder encoderOne = intakeMotorOne.getEncoder();
+        one.intakeMotorVelocity = encoderOne.getPosition();
+        one.intakeMotorPosition = encoderOne.getVelocity();
+        return one;
+    }
+
+    public IntakeInfo getIntakeMotorTwoInfo() {
+        IntakeInfo two = new IntakeInfo();
+        RelativeEncoder encoderTwo = intakeMotorTwo.getEncoder();
+        two.intakeMotorVelocity = encoderTwo.getPosition();
+        two.intakeMotorPosition = encoderTwo.getVelocity();
+        return two;
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Intake Bottom Motor Voltage", intakeMotorOne.getMotorVoltage().getValue());
-        SmartDashboard.putNumber("Intake Bottom Position", intakeMotorOne.getPosition().getValue());
-        SmartDashboard.putNumber("Intake Top Motor Voltage", intakeMotorTwo.getMotorVoltage().getValue());
-        SmartDashboard.putNumber("Intake Top Position", intakeMotorTwo.getPosition().getValue());
+        double appliedOutputOne = intakeMotorOne.getAppliedOutput();
+        double busVoltageOne = intakeMotorOne.getBusVoltage();
+        double motorVoltageOne = appliedOutputOne * busVoltageOne;
+        double appliedOutputTwo = intakeMotorTwo.getAppliedOutput();
+        double busVoltageTwo = intakeMotorTwo.getBusVoltage();
+        double motorVoltageTwo = appliedOutputTwo * busVoltageTwo;
+
+        RelativeEncoder encoderOne = intakeMotorOne.getEncoder();
+        RelativeEncoder encoderTwo = intakeMotorTwo.getEncoder();
+        Double posOne = encoderOne.getPosition();
+        Double posTwo = encoderTwo.getPosition();
+
+        SmartDashboard.putNumber("Intake Bottom Motor Voltage", motorVoltageOne);
+        SmartDashboard.putNumber("Intake Bottom Position", posOne);
+        SmartDashboard.putNumber("Intake Top Motor Voltage", motorVoltageTwo);
+        SmartDashboard.putNumber("Intake Top Position", posTwo);
   }
 }
 
