64001ff439d3f5ceabc3cd3f86ab3340ac9f5e73
shlokacharya <64059790+shlokacharya@users.noreply.github.com>
2024-12-10 10:05:30 -0800

    Intake Subsystem + Command

---
+++ src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+  private final CANSparkMax leftMotor;
+  private final CANSparkMax rightMotor;
+  public IntakeSubsystem() {
+    leftMotor = new CANSparkMax(8,MotorType.kBrushless);
+    rightMotor = new CANSparkMax(9, MotorType.kBrushless);
+  }
+  public void setSpeed(double speed){
+    leftMotor.set(speed);
+    rightMotor.set(speed);
+  }
+  public void brake(){
+    leftMotor.setIdleMode(IdleMode.kBrake);
+    rightMotor.setIdleMode(IdleMode.kBrake);
+  }
+  public void coast(){
+    leftMotor.setIdleMode(IdleMode.kCoast);
+    rightMotor.setIdleMode(IdleMode.kCoast);
+  }
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Left Intake Motor Temp: ", leftMotor.getMotorTemperature());
+    SmartDashboard.putNumber("Right Intake Motor Temp: ", rightMotor.getMotorTemperature());
+    SmartDashboard.putNumber("Left Intake Motor Voltage: ", leftMotor.getBusVoltage());
+    SmartDashboard.putNumber("Right Intake Motor Voltage: ", rightMotor.getBusVoltage());
+  }
+}
