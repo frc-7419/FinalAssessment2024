2f73d3268c9ab17cb3eb7375f80079972c67326d
Ary <ary10kum@gmail.com>
2024-12-10 22:54:47 -0800

    Swerve

---
+++ src/main/java/frc/robot/subsystems/SwerveBase.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveBase extends SubsystemBase {
+  private final SwerveModule backLeft;
+  private final SwerveModule backRight;
+  private final SwerveModule frontLeft;
+  private final SwerveModule frontRight;
+  private final Pigeon2 gyroscope;
+  public SwerveBase() {
+    backLeft = new SwerveModule(new TalonFX(4), new TalonFX(5));
+    backRight = new SwerveModule(new TalonFX(6), new TalonFX(7));
+     frontLeft = new SwerveModule(new TalonFX(8), new TalonFX(1));
+    frontRight = new SwerveModule(new TalonFX(2), new TalonFX(3));
+    gyroscope = new Pigeon2(0);  
+  }
+  public void setSwerveWithJoystick(double vx, double vy, double rx){
+    setChassisSpeed(ChassisSpeeds.fromFieldRelativeSpeeds(vx,vy,rx,gyroscope.getRotation2d()));
+  }
+  public void setChassisSpeed(ChassisSpeeds chassisSpeeds) {
+    setSwerveStates(new SwerveDriveKinematics().toSwerveModuleStates(chassisSpeeds));
+  }
+  public void setSwerveStates(SwerveModuleState[] states){
+    backLeft.setSwerveState(states[0]); 
+    backRight.setSwerveState(states[1]);
+    frontLeft.setSwerveState(states[2]);
+    frontRight.setSwerveState(states[3]);
+  }
+
+  public void coast(){
+    backLeft.coast();
+    backRight.coast();   
+    frontLeft.coast();
+    frontRight.coast(); 
+  }
+
+  public void brake(){
+    backLeft.brake();
+    backRight.brake(); 
+    frontLeft.brake();
+    frontRight.brake();   
+  } 
+
+
+  @Override
+  public void periodic() {
+    
+    SmartDashboard.putNumber("Back Left module angle", backLeft.getAngle());
+    SmartDashboard.putNumber("Back Right module angle", backRight.getAngle());
+    SmartDashboard.putNumber("Front Left module angle", frontLeft.getAngle());
+    SmartDashboard.putNumber("Front Right module angle", frontRight.getAngle());
+   
+    SmartDashboard.putNumber("Back Left module speed", backLeft.getSpeed());    
+    SmartDashboard.putNumber("Back Right module speed", backRight.getSpeed());
+    SmartDashboard.putNumber("Front Left module speed", frontLeft.getSpeed());
+    SmartDashboard.putNumber("Front Right module speed", frontRight.getSpeed());
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveModule extends SubsystemBase {
+  private final TalonFX turnMotor;
+  private final TalonFX moveMotor;
+  private final PIDController angController;
+  private final CANcoder encoder;
+  private double speed;
+  public SwerveModule(TalonFX turnMotor, TalonFX moveMotor) {
+    this.turnMotor = turnMotor;
+    this.moveMotor = moveMotor;
+    encoder = new CANcoder(11);
+    angController = new PIDController(0.1, 0, 0);
+    angController.setTolerance(1);
+    speed = 0;
+  }
+
+  public void setSwerveState(SwerveModuleState swerveState) {
+    speed = swerveState.speedMetersPerSecond;
+    moveMotor.set(swerveState.speedMetersPerSecond);
+    angController.setSetpoint(swerveState.angle.getDegrees());
+    angController.calculate(turnMotor.getPosition().getValueAsDouble());
+  }
+  public void coast(){
+    turnMotor.setNeutralMode(NeutralModeValue.Coast);
+    moveMotor.setNeutralMode(NeutralModeValue.Coast);
+  }
+  public void brake(){
+    turnMotor.setNeutralMode(NeutralModeValue.Brake);
+    moveMotor.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+  public double getAngle(){
+    return encoder.getPosition().getValueAsDouble();
+  }
+  public double getSpeed(){
+    return speed;
+  }
+
+
+  @Override
+  public void periodic() {
+  }
+}
