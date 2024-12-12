fc514c211543a3fdcb51127b488356f7a4ed75d6
shlokacharya <64059790+shlokacharya@users.noreply.github.com>
2024-12-12 13:22:07 -0800

    Naming Change

--- src/main/java/frc/robot/Commands/RunShooterJoystick.java
+++ src/main/java/frc/robot/Commands/RunShooterJoystick.java
@@ -2,11 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Commands;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Subsystems.ShooterSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class RunShooterJoystick extends Command {
   private final ShooterSubsystem shooterSubsystem;
