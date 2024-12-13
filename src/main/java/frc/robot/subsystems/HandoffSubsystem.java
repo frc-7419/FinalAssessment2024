c3c7bf2a55f0b93485c1999337cf1f84912cf5eb
mqxs21 <maxshi239@gmail.com>
2024-12-12 17:25:37 -0800

    remove unused imports

--- src/main/java/frc/robot/subsystems/HandoffSubsystem.java
+++ src/main/java/frc/robot/subsystems/HandoffSubsystem.java
@@ -4,8 +4,7 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
