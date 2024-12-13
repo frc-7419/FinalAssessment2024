2ce9c970c95198da0fc543d3de45c789c79ad262
Vihaan <vihaan.siramdas123@gmail.com>
2024-12-13 13:51:28 -0800

    HangerSystem
    Apparently this didnt push so im pushing it now. Sigma Sigma Boy

--- src/main/java/frc/robot/Subsystems/HangerSubsystem.java
+++ src/main/java/frc/robot/Subsystems/HangerSubsystem.java
@@ -5,6 +5,9 @@
 package frc.robot.Subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class HangerSubsystem extends SubsystemBase {
@@ -13,11 +16,22 @@ public class HangerSubsystem extends SubsystemBase {
   public HangerSubsystem() {
     this.hangerMotor = new TalonFX(2);
 
+
   }
-  
+  public void coast(){
+    hangerMotor.setNeutralMode(NeutralModeValue.Coast);
+  }
+  public void run(double power){
+    hangerMotor.set(power);
+  }
+  public void brake(){
+    hangerMotor.setNeutralMode(NeutralModeValue.Brake);   
+  }
+    
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Hanger Temp", hangerMotor.getDeviceTemp().getValueAsDouble());
     // This method will be called once per scheduler run
   }
 }
