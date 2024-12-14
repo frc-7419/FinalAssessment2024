e00a81f39e3d640547c2d554ebc707ffa238bbd2
Eshan41 <eshanpatel.genius@gmail.com>
2024-12-13 21:46:48 -0800

    Finished Intake Subsystem and got venderdrops working

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -6,23 +6,35 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new IntakeSubsystem. */
-  private final CANSparkMax topMotor;
-
+  private final CANSparkMax motorOne;
+  private final CANSparkMax motorTwo;
   public IntakeSubsystem() {
-    topMotor = new CANSparkMax(0, MotorType.kBrushless);
-    
-
-
+    motorOne = new CANSparkMax(0, MotorType.kBrushless);
+    motorTwo = new CANSparkMax(0, MotorType.kBrushless);
   }
-
+  public void coast(){
+    motorOne.setIdleMode(IdleMode.kCoast);
+    motorTwo.setIdleMode(IdleMode.kCoast);
+  }
+  public void brake(){
+    motorOne.setIdleMode(IdleMode.kBrake);
+    motorTwo.setIdleMode(IdleMode.kBrake);
+  }
+  public void setSpeed(double speed){
+    motorOne.set(speed);
+    motorTwo.set(speed);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-
+    SmartDashboard.putNumber("Intake Motor One Temperature", motorOne.getMotorTemperature());
+    SmartDashboard.putNumber("Intake Motor Two Temperature", motorTwo.getMotorTemperature());
   }
 }
