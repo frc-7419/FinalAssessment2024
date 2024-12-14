b45859a7cab0a0c0dec898afd4212581fc3d3861
Eshan41 <eshanpatel.genius@gmail.com>
2024-12-13 21:59:18 -0800

    Did handoff subsystem

--- src/main/java/frc/robot/subsystems/HandoffSubsystem.java
+++ src/main/java/frc/robot/subsystems/HandoffSubsystem.java
@@ -5,15 +5,37 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class HandoffSubsystem extends SubsystemBase {
   /** Creates a new HandoffSubsystem. */
+  private final CANSparkMax topMotor;
+  private final CANSparkMax bottomMotor;
   public HandoffSubsystem() {
-    
+    topMotor = new CANSparkMax(0, MotorType.kBrushless);
+    bottomMotor = new CANSparkMax(0, MotorType.kBrushless);
+    bottomMotor.setInverted(true);
   }
-
+  public void coast(){
+    topMotor.setIdleMode(IdleMode.kCoast);
+    bottomMotor.setIdleMode(IdleMode.kCoast);
+  }
+  public void brake(){
+    topMotor.setIdleMode(IdleMode.kBrake);
+    bottomMotor.setIdleMode(IdleMode.kBrake);
+  }
+  public void setSpeed(double speed){
+    topMotor.set(speed);
+    bottomMotor.set(speed);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber(" Top handoff motor temperature", topMotor.getMotorTemperature());
+    SmartDashboard.putNumber("Bottom hand off motor temperature ", bottomMotor.getMotorTemperature());
   }
 }
