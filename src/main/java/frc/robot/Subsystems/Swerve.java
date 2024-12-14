4048c20d24a9770dc466b12f10c04713286ed2b8
Arhaan28 <arhaanreddy@gmail.com>
2024-12-13 21:48:28 -0800

    I don't know how to do swerve

--- src/main/java/frc/robot/Subsystems/Swerve.java
+++ src/main/java/frc/robot/Subsystems/Swerve.java
@@ -1,5 +1,14 @@
 package frc.robot.Subsystems;
 
 public class Swerve {
+    // Define the motors
+
+    // Setting power to the motors
+
+    // Setting the motors to coast
+
+    // Setting the motors to brake
+
+    // Uploading motor info the Smartdashboard
     
 }
